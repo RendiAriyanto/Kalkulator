fd69b217b97f36e6e873cae7dec7007ade6d937e
RendiAriyanto <95177099+RendiAriyanto@users.noreply.github.com>
2021-12-17 10:13:40 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const number = document.querySelectorAll(".number");
+
+const numbers.forEach((number) => {
+    numbers.addEventListener("click", ()=>{
+        console.log("event.target.value");
+    });
+});
+
