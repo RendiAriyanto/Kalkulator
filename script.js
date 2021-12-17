4ab845602a77d915f81d940b2b72cde7a0ef7847
RendiAriyanto <95177099+RendiAriyanto@users.noreply.github.com>
2021-12-17 14:47:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const updateScreen = (number) =>{
     calculatorScreen.value = number;
 }
 
-let prevNumber = ``;
-let calculationOperator = ``;
-let currentNumber = `0`;
+
 const numbers = document.querySelectorAll(".number");
-const inputNumber = (number) =>{
- curentNumber += number;   
-}
+
     numbers.forEach((number) => {
     numbers.addEventListener("click", (event)=>{
         console.log(event.target.value);
         updateScreen(currentNumber);
     });
 });
+let prevNumber = ``;
+let calculationOperator = ``;
+let currentNumber = `0`;
+const inputNumber = (number) =>{
+ curentNumber += number;   
+}
